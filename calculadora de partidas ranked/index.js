e5894e24c14513b15fc64b60a671638784964cb0
Lucas Pereira <lucas_rm35@hotmail.com>
2024-02-26 07:24:53 -0400

    criando novo projeto
    projeto que calcula saldo de vitoria e retorna o nivel do usuario

---
+++ calculadora de partidas ranked/index.js
@@ -0,0 +1,10 @@
+
+
+function saldoDeVitorias(qntVitoria, qntDerrota) {
+    let saldoVitoria = qntVitoria - qntDerrota;
+    return (`o salado de vitoria é ${saldoVitoria} e o nivel `);
+    
+
+}
+console.log(saldoDeVitorias(15,3));
+
